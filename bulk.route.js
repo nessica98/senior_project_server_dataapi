a4dd16ba02b6caa7fe5acc6b8fa426447966f721
nessica98 <poksin.w@gmail.com>
2021-02-15 11:46:42 +0700

    C:/Program Files/Git/_bulk update

--- bulk.route.js
+++ bulk.route.js
@@ -4,11 +4,13 @@ const moment = require('moment')
 const db = require('./models')
 const sequelize = db.sequelize
 const GatewayData = db.GatewayData
+const LogbookData = db.LogbookData
+const LogbookListData = db.LogbookListData
 const nodeGPSdata = db.nodegpsdata
 const nodedata = db.nodedata
 
 // server listen : add to server db
-router.post('/post',async (req,res)=>{
+router.post('/gps-post',async (req,res)=>{
     const {gatewayId,last_update,payload} = req.body
     if(!(gatewayId && last_update && payload)) {
         console.log('aaa')
@@ -23,7 +25,7 @@ router.post('/post',async (req,res)=>{
     var nodelist_new = new Set()
     payload.forEach((val,idx)=>{
         console.log('add to db', val);
-        nodelist_new.add(val.nodeName)
+        nodelist_new.add(val.nodename)
         var new_GPSrow = nodeGPSdata.build(val)
         new_GPSrow.save().then((result)=>{
             console.log()
@@ -40,6 +42,39 @@ router.post('/post',async (req,res)=>{
       })
     })
     res.send('add to server DB')
+})
+
+router.post('/logbook-post',async (req,res)=>{
+    const {gatewayId,last_update,payload} = req.body
+    if(!(gatewayId && last_update && payload)) {
+        console.log('aaa')
+        res.sendStatus(400);
+        return;
+    }
+    if(!Array.isArray(payload)){
+        console.log('bbb')
+        res.sendStatus(400)
+        return;
+    }
+    var nodelist_new = new Set()
+    payload.forEach((val,idx)=>{
+        console.log('add to db', val);
+        nodelist_new.add(val.nodename)
+        LogbookData.create(val, { include: [{ model: LogbookListData, as: 'lists' }] }).then((val)=>{
+            res.send({status:'complete'})
+        }).catch((err)=>{
+            res.sendStatus(500)
+        })
+    })
+    nodelist_new.forEach((val)=>{
+        console.log(val)
+        nodedata.update({nodeupdate:new Date()},{ 
+        where: {
+          nodename:val
+        }
+      })
+    })
+    //res.send('add to server DB')
 })
 
 module.exports = router
