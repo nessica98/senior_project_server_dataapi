cdd6b1a2fa4ac724e9250b61c2eea2023ad5fc3f
Jisung Hwang <poksin.w@gmail.com>
2021-02-24 16:54:38 +0700

    edit /api add more route

--- node.route.js
+++ node.route.js
@@ -3,6 +3,19 @@ const db = require('./models')
 const NodeData = db.nodedata
 const NodeOwner = db.NodeOwner
 
+router.post('/adduser', (req,res)=>{
+    const {nodeownername,raw_password} = req.body;
+    const newowner = {NodeOwnerId:uuidv4(),NodeOwnerName:nodeownername, NodeOwnerKeyHd5:raw_password}
+    NodeOwner.create(newowner).then((result)=>{
+        res.send({status:"add complete",data:result})
+    }).catch((reason)=>{
+        console.error(reason)
+        
+        res.status(500).send({status:"error",reason:reason})
+    })
+    
+})
+
 router.post('/add', async (req,res)=>{
     const {nodename,node_startdate,nodeowner } = req.body
     if(!(nodename && node_startdate && nodeowner)) {
@@ -11,7 +24,7 @@ router.post('/add', async (req,res)=>{
     }
     const ownerid = await NodeOwner.findOne({where: {NodeOwnerName:nodeowner},raw:true}, {attribute:['NodeOwnerId']})
     console.log(ownerid.NodeOwnerId)
-    if(ownerid){
+    if(ownerid.NodeOwnerId){
         const newnode = {nodename:nodename,nodestartwork:new Date(node_startdate),nodeackupdate:new Date(),nodeupdate:new Date(),nodeownerNodeOwnerId:ownerid.NodeOwnerId,NodeOwnerId:ownerid}
         NodeData.create(newnode).then((result)=>{
             res.send({status:"add complete",data:result})
